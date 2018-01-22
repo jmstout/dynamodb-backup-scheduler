fa30fdea1f3f96e0d040d0b4829b502b295d6d64
Eryk Warren <emw@mwarren.org>
2018-01-21 19:57:54 -0500

    Simplifly code. Use Bluebird

--- handler.ts
+++ handler.ts
@@ -1,68 +1,51 @@
 import { DynamoDB } from 'aws-sdk';
 import * as qs from 'qs';
+import * as Bluebird from 'bluebird';
 
 const dynamoDb = new DynamoDB();
 
-function batch(array: any[], size: number): any[][] {
-  const result = [];
-  let index = 0;
-
-  while (index < array.length) {
-    result.push(array.slice(index, index + size));
-    index += size;
+async function createBackup(tableName: string, backupName: string): Promise<void> {
+  try {
+    await dynamoDb.createBackup({
+      TableName: tableName,
+      BackupName: backupName,
+    }).promise();
+    console.log(`Backed up ${tableName}`);
+  } catch (err) {
+    console.error(`Could not backup ${tableName}`, err);
   }
-
-  return result;
 }
 
-const makeLogCallback = (successMsg: string, errMsg: string) => (resolve, reject) => (err, data) => {
-  if (err) {
-    console.error(errMsg, err);
-    resolve(err);
-  }
-  console.log(successMsg);
-  resolve(data);
-};
-
-function createBackup(tableName: string, backupName: string): Promise<void> {
-  const params = {
-    TableName: tableName,
-    BackupName: backupName,
-  };
-  const logCallback = makeLogCallback(
-    `Backed up ${tableName}`,
-    `Could not backup ${tableName}`,
-  );
-  return new Promise<void>((resolve, reject) => dynamoDb.createBackup(params, logCallback(resolve, reject)));
-}
-
-async function getBackupList(tableNames: string[], fromDate: Date, toDate: Date): Promise<DynamoDB.Types.BackupSummary[]> {
-  const params = {
-    TimeRangeLowerBound: fromDate,
-    TimeRangeUpperBound: toDate,
-  };
-  const logCallback = makeLogCallback(
-    `Got existing list of backups`,
-    `Could not get list of backups`,
-  );
+async function getBackupList(
+  tableNames: string[],
+  fromDate: Date,
+  toDate: Date,
+): Promise<DynamoDB.Types.BackupSummary[]> {
   // TODO: handle multiple pages
-  const allBackups: DynamoDB.Types.ListBackupsOutput = await new Promise(
-    (resolve, reject) =>
-      dynamoDb.listBackups(params, logCallback(resolve, reject)),
-  );
-  // return only the backup for our tables
-  return allBackups.BackupSummaries
-    .filter(summary => tableNames.includes(summary.TableName));
+  try {
+    const allBackups: DynamoDB.Types.ListBackupsOutput =
+      await dynamoDb.listBackups({
+        TimeRangeLowerBound: fromDate,
+        TimeRangeUpperBound: toDate,
+      }).promise();
+    console.log('Got existing list of backups');
+    // return only the backup for our tables
+    return allBackups.BackupSummaries
+      .filter(summary => tableNames.includes(summary.TableName));
+  } catch (err) {
+    console.error('Could not get list of backups:', err);
+  }
 }
 
 async function deleteBackup(backupArn: string, tableName: string): Promise<void> {
-  const logCallback = makeLogCallback(
-    `Deleted backup of ${tableName}: ${backupArn}`,
-    `Could not delete backup of ${tableName} ${backupArn}`,
-  );
-  return new Promise<void>((resolve, reject) => dynamoDb.deleteBackup({
-    BackupArn: backupArn,
-  }, logCallback(resolve, reject)));
+  try {
+    await dynamoDb.deleteBackup({
+      BackupArn: backupArn,
+    }).promise();
+    console.log(`Deleted backup of ${tableName}: ${backupArn}`);
+  } catch (err) {
+    console.error(`Could not delete backup of ${tableName} ${backupArn}`, err);
+  }
 }
 
 
@@ -77,12 +60,11 @@ export async function backupDynamoDBTables(event, context, callback): Promise<vo
 
   // backup all the tables in parallel, 50 at time.
   // CreateBackup can be called at a maximum rate of 50 times per second.
-  const createBackupForTable = tableName => createBackup(tableName, backupName);
-  const batchedTableNames = batch(tableNames, 50);
-  for (const currentBatch of batchedTableNames) {
-    console.log('...processing currentBatch', currentBatch);
-    await Promise.all(currentBatch.map(createBackupForTable));
-  }
+  await Bluebird.map(
+    tableNames,
+    (tableName: string) => createBackup(tableName, backupName),
+    { concurrency: 50 },
+  );
 
   // remove old backups
   const fromDate = new Date('2017-01-01');
